d575800125e4f9f9d302e03747fc5c8fd84404b5
Bui-Duc-Trung <157358168+Bui-Duc-Trung@users.noreply.github.com>
2024-07-15 09:38:27 +0700

    Hoàn thành form QL nhân viên

--- GUI/FormIndex.Designer.cs
+++ GUI/FormIndex.Designer.cs
@@ -52,16 +52,16 @@
             panel1.ForeColor = SystemColors.ButtonHighlight;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1408, 65);
+            panel1.Size = new Size(1443, 61);
             panel1.TabIndex = 0;
             // 
             // button2
             // 
             button2.BackColor = Color.Black;
             button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button2.Location = new Point(1331, 1);
+            button2.Location = new Point(1374, 1);
             button2.Name = "button2";
-            button2.Size = new Size(72, 59);
+            button2.Size = new Size(66, 55);
             button2.TabIndex = 2;
             button2.Text = "X";
             button2.UseVisualStyleBackColor = false;
@@ -79,9 +79,9 @@
             flowLayoutPanel1.Controls.Add(btn_thongTinTK);
             flowLayoutPanel1.Controls.Add(btn_dangXuat);
             flowLayoutPanel1.Dock = DockStyle.Left;
-            flowLayoutPanel1.Location = new Point(0, 65);
+            flowLayoutPanel1.Location = new Point(0, 61);
             flowLayoutPanel1.Name = "flowLayoutPanel1";
-            flowLayoutPanel1.Size = new Size(240, 770);
+            flowLayoutPanel1.Size = new Size(222, 704);
             flowLayoutPanel1.TabIndex = 1;
             flowLayoutPanel1.Paint += flowLayoutPanel1_Paint;
             // 
@@ -92,7 +92,7 @@
             btn_hoaDon.ForeColor = SystemColors.ButtonHighlight;
             btn_hoaDon.Location = new Point(3, 3);
             btn_hoaDon.Name = "btn_hoaDon";
-            btn_hoaDon.Size = new Size(225, 80);
+            btn_hoaDon.Size = new Size(208, 75);
             btn_hoaDon.TabIndex = 0;
             btn_hoaDon.Text = "\U0001f6d2Hóa đơn";
             btn_hoaDon.UseVisualStyleBackColor = false;
@@ -103,9 +103,9 @@
             btn_sanPham.BackColor = SystemColors.ActiveCaptionText;
             btn_sanPham.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btn_sanPham.ForeColor = SystemColors.ButtonHighlight;
-            btn_sanPham.Location = new Point(3, 89);
+            btn_sanPham.Location = new Point(3, 84);
             btn_sanPham.Name = "btn_sanPham";
-            btn_sanPham.Size = new Size(225, 80);
+            btn_sanPham.Size = new Size(208, 75);
             btn_sanPham.TabIndex = 5;
             btn_sanPham.Text = "Sản phẩm";
             btn_sanPham.UseVisualStyleBackColor = false;
@@ -116,9 +116,9 @@
             btn_khuyenMai.BackColor = SystemColors.ActiveCaptionText;
             btn_khuyenMai.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btn_khuyenMai.ForeColor = SystemColors.ButtonHighlight;
-            btn_khuyenMai.Location = new Point(3, 175);
+            btn_khuyenMai.Location = new Point(3, 165);
             btn_khuyenMai.Name = "btn_khuyenMai";
-            btn_khuyenMai.Size = new Size(225, 80);
+            btn_khuyenMai.Size = new Size(208, 75);
             btn_khuyenMai.TabIndex = 6;
             btn_khuyenMai.Text = "Khuyến mãi";
             btn_khuyenMai.UseVisualStyleBackColor = false;
@@ -129,9 +129,9 @@
             btn_quanLyNV.BackColor = SystemColors.ActiveCaptionText;
             btn_quanLyNV.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btn_quanLyNV.ForeColor = SystemColors.ButtonHighlight;
-            btn_quanLyNV.Location = new Point(3, 261);
+            btn_quanLyNV.Location = new Point(3, 246);
             btn_quanLyNV.Name = "btn_quanLyNV";
-            btn_quanLyNV.Size = new Size(225, 115);
+            btn_quanLyNV.Size = new Size(208, 108);
             btn_quanLyNV.TabIndex = 7;
             btn_quanLyNV.Text = "Quản lý nhân viên";
             btn_quanLyNV.UseVisualStyleBackColor = false;
@@ -142,9 +142,9 @@
             btn_quanLyKH.BackColor = SystemColors.ActiveCaptionText;
             btn_quanLyKH.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btn_quanLyKH.ForeColor = SystemColors.ButtonHighlight;
-            btn_quanLyKH.Location = new Point(3, 382);
+            btn_quanLyKH.Location = new Point(3, 360);
             btn_quanLyKH.Name = "btn_quanLyKH";
-            btn_quanLyKH.Size = new Size(225, 115);
+            btn_quanLyKH.Size = new Size(208, 108);
             btn_quanLyKH.TabIndex = 8;
             btn_quanLyKH.Text = "Quản lý khách hàng";
             btn_quanLyKH.UseVisualStyleBackColor = false;
@@ -155,9 +155,9 @@
             btn_thongTinTK.BackColor = SystemColors.ActiveCaptionText;
             btn_thongTinTK.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btn_thongTinTK.ForeColor = SystemColors.ButtonHighlight;
-            btn_thongTinTK.Location = new Point(3, 503);
+            btn_thongTinTK.Location = new Point(3, 474);
             btn_thongTinTK.Name = "btn_thongTinTK";
-            btn_thongTinTK.Size = new Size(225, 115);
+            btn_thongTinTK.Size = new Size(208, 108);
             btn_thongTinTK.TabIndex = 9;
             btn_thongTinTK.Text = "Thông tin tài khoản";
             btn_thongTinTK.UseVisualStyleBackColor = false;
@@ -168,9 +168,9 @@
             btn_dangXuat.BackColor = SystemColors.ActiveCaptionText;
             btn_dangXuat.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btn_dangXuat.ForeColor = SystemColors.ButtonHighlight;
-            btn_dangXuat.Location = new Point(3, 624);
+            btn_dangXuat.Location = new Point(3, 588);
             btn_dangXuat.Name = "btn_dangXuat";
-            btn_dangXuat.Size = new Size(225, 115);
+            btn_dangXuat.Size = new Size(208, 108);
             btn_dangXuat.TabIndex = 10;
             btn_dangXuat.Text = "Đăng xuất";
             btn_dangXuat.UseVisualStyleBackColor = false;
@@ -180,17 +180,17 @@
             // 
             pn_Body.BackgroundImageLayout = ImageLayout.None;
             pn_Body.Dock = DockStyle.Right;
-            pn_Body.Location = new Point(270, 65);
+            pn_Body.Location = new Point(213, 61);
             pn_Body.Name = "pn_Body";
-            pn_Body.Size = new Size(1138, 770);
+            pn_Body.Size = new Size(1230, 704);
             pn_Body.TabIndex = 3;
             pn_Body.Paint += pn_Body_Paint;
             // 
             // FormIndex
             // 
-            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleDimensions = new SizeF(12F, 30F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1408, 835);
+            ClientSize = new Size(1443, 765);
             Controls.Add(pn_Body);
             Controls.Add(flowLayoutPanel1);
             Controls.Add(panel1);

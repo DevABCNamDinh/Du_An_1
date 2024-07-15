92e6151959908b8c38fa04b60763bbf855876336
Admin <Admin@DESKTOP-3HK4GP0>
2024-07-15 10:32:20 +0700

    Hoàn thành form

--- GUI/FormQLKhachHang.Designer.cs
+++ GUI/FormQLKhachHang.Designer.cs
@@ -28,19 +28,281 @@
         /// </summary>
         private void InitializeComponent()
         {
+            txt_search = new TextBox();
+            label9 = new Label();
+            btn_clean = new Button();
+            btn_delete = new Button();
+            btn_add = new Button();
+            btn_update = new Button();
+            txt_email = new TextBox();
+            txt_sdt = new TextBox();
+            txt_diachi = new TextBox();
+            txt_khachhang = new TextBox();
+            txt_id = new TextBox();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            dtg_hienthi = new DataGridView();
+            label1 = new Label();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            ((System.ComponentModel.ISupportInitialize)dtg_hienthi).BeginInit();
             SuspendLayout();
+            // 
+            // txt_search
+            // 
+            txt_search.Location = new Point(711, 236);
+            txt_search.Name = "txt_search";
+            txt_search.Size = new Size(299, 31);
+            txt_search.TabIndex = 41;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(614, 239);
+            label9.Name = "label9";
+            label9.Size = new Size(64, 25);
+            label9.TabIndex = 40;
+            label9.Text = "Search";
+            // 
+            // btn_clean
+            // 
+            btn_clean.Location = new Point(687, 366);
+            btn_clean.Name = "btn_clean";
+            btn_clean.Size = new Size(135, 59);
+            btn_clean.TabIndex = 39;
+            btn_clean.Text = "CLEAN";
+            btn_clean.UseVisualStyleBackColor = true;
+            // 
+            // btn_delete
+            // 
+            btn_delete.Location = new Point(865, 366);
+            btn_delete.Name = "btn_delete";
+            btn_delete.Size = new Size(118, 59);
+            btn_delete.TabIndex = 38;
+            btn_delete.Text = "DELETE";
+            btn_delete.UseVisualStyleBackColor = true;
+            // 
+            // btn_add
+            // 
+            btn_add.Location = new Point(687, 291);
+            btn_add.Name = "btn_add";
+            btn_add.Size = new Size(135, 59);
+            btn_add.TabIndex = 37;
+            btn_add.Text = "ADD";
+            btn_add.UseVisualStyleBackColor = true;
+            // 
+            // btn_update
+            // 
+            btn_update.Location = new Point(865, 291);
+            btn_update.Name = "btn_update";
+            btn_update.Size = new Size(118, 59);
+            btn_update.TabIndex = 36;
+            btn_update.Text = "UPDATE";
+            btn_update.UseVisualStyleBackColor = true;
+            // 
+            // txt_email
+            // 
+            txt_email.Location = new Point(711, 173);
+            txt_email.Name = "txt_email";
+            txt_email.Size = new Size(299, 31);
+            txt_email.TabIndex = 35;
+            // 
+            // txt_sdt
+            // 
+            txt_sdt.Location = new Point(711, 112);
+            txt_sdt.Name = "txt_sdt";
+            txt_sdt.Size = new Size(299, 31);
+            txt_sdt.TabIndex = 34;
+            // 
+            // txt_diachi
+            // 
+            txt_diachi.Location = new Point(243, 302);
+            txt_diachi.Name = "txt_diachi";
+            txt_diachi.Size = new Size(299, 31);
+            txt_diachi.TabIndex = 33;
+            // 
+            // txt_khachhang
+            // 
+            txt_khachhang.Location = new Point(243, 176);
+            txt_khachhang.Name = "txt_khachhang";
+            txt_khachhang.Size = new Size(299, 31);
+            txt_khachhang.TabIndex = 31;
+            // 
+            // txt_id
+            // 
+            txt_id.Location = new Point(243, 106);
+            txt_id.Name = "txt_id";
+            txt_id.Size = new Size(299, 31);
+            txt_id.TabIndex = 30;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(614, 176);
+            label8.Name = "label8";
+            label8.Size = new Size(54, 25);
+            label8.TabIndex = 29;
+            label8.Text = "Email";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(614, 112);
+            label7.Name = "label7";
+            label7.Size = new Size(43, 25);
+            label7.TabIndex = 28;
+            label7.Text = "SDT";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(31, 403);
+            label6.Name = "label6";
+            label6.Size = new Size(190, 25);
+            label6.TabIndex = 27;
+            label6.Text = "Danh sách khách hàng";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(52, 308);
+            label5.Name = "label5";
+            label5.Size = new Size(65, 25);
+            label5.TabIndex = 26;
+            label5.Text = "Địa chỉ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(52, 247);
+            label4.Name = "label4";
+            label4.Size = new Size(78, 25);
+            label4.TabIndex = 25;
+            label4.Text = "Giới tính";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(52, 182);
+            label3.Name = "label3";
+            label3.Size = new Size(134, 25);
+            label3.TabIndex = 24;
+            label3.Text = "Tên khách hàng";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(52, 112);
+            label2.Name = "label2";
+            label2.Size = new Size(127, 25);
+            label2.TabIndex = 23;
+            label2.Text = "ID Khách hàng";
+            // 
+            // dtg_hienthi
+            // 
+            dtg_hienthi.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dtg_hienthi.Location = new Point(31, 431);
+            dtg_hienthi.Name = "dtg_hienthi";
+            dtg_hienthi.RowHeadersWidth = 62;
+            dtg_hienthi.Size = new Size(989, 194);
+            dtg_hienthi.TabIndex = 22;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(31, 56);
+            label1.Name = "label1";
+            label1.Size = new Size(133, 25);
+            label1.TabIndex = 21;
+            label1.Text = "Nhập thông tin";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(243, 247);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(75, 29);
+            radioButton1.TabIndex = 42;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Nam";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(404, 247);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(61, 29);
+            radioButton2.TabIndex = 43;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Nữ";
+            radioButton2.UseVisualStyleBackColor = true;
             // 
             // FormQLKhachHang
             // 
-            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1051, 681);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(txt_search);
+            Controls.Add(label9);
+            Controls.Add(btn_clean);
+            Controls.Add(btn_delete);
+            Controls.Add(btn_add);
+            Controls.Add(btn_update);
+            Controls.Add(txt_email);
+            Controls.Add(txt_sdt);
+            Controls.Add(txt_diachi);
+            Controls.Add(txt_khachhang);
+            Controls.Add(txt_id);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(dtg_hienthi);
+            Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(2);
             Name = "FormQLKhachHang";
             Text = "FormQLKhachHang";
+            Load += FormQLKhachHang_Load;
+            ((System.ComponentModel.ISupportInitialize)dtg_hienthi).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txt_search;
+        private Label label9;
+        private Button btn_clean;
+        private Button btn_delete;
+        private Button btn_add;
+        private Button btn_update;
+        private TextBox txt_email;
+        private TextBox txt_sdt;
+        private TextBox txt_diachi;
+        private TextBox txt_khachhang;
+        private TextBox txt_id;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private DataGridView dtg_hienthi;
+        private Label label1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
     }
 }
